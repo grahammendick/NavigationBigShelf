0eda9ef1c8296556d856017038cc5801766deb84
Graham Mendick <graham.mendick@gmail.com>
2012-05-28 11:16:12 +0100

    Added more css and layout

--- BigShelf/Views/Shared/Layout.Master.designer.cs
+++ BigShelf/Views/Shared/Layout.Master.designer.cs
@@ -22,6 +22,24 @@ namespace BigShelf.Views.Shared {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// homeLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Navigation.NavigationHyperLink homeLink;
+        
+        /// <summary>
+        /// headerImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image headerImage;
+        
+        /// <summary>
         /// Content control.
         /// </summary>
         /// <remarks>
