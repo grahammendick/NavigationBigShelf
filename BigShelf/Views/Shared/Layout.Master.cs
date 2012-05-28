dc03250db487ac3dca0ac04bb4fa47deda52b643
Graham Mendick <graham.mendick@gmail.com>
2012-05-28 15:26:27 +0100

    Tidied up Usings

--- BigShelf/Views/Shared/Layout.Master.cs
+++ BigShelf/Views/Shared/Layout.Master.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using System.Web.UI;
 
 namespace BigShelf.Views.Shared
 {
-	public partial class Layout : System.Web.UI.MasterPage
+	public partial class Layout : MasterPage
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-
-		}
 	}
 }
